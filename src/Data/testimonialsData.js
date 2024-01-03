f9a16e3fccf97a8e776c875edd39b79c94e0d25f
sankettripathy <128065019+sankettripathy@users.noreply.github.com>
2024-01-03 22:24:08 +0530

    Add files via upload

---
+++ src/Data/testimonialsData.js
@@ -0,0 +1,25 @@
+import image1 from "../assets/t-image1.png";
+import image2 from "../assets/t-image2.jpg";
+import image3 from "../assets/t-image3.jpg";
+
+export const testimonialsData = [
+  {
+    image: image1,
+    review:
+      "I made the right choice by choosing the Fitclub and by choosing the right plan and program I already achieved my ideal body!",
+    name: 'MATHEW HENDRICKSON',
+    status : 'ENTREPRENEUR'
+  },
+  {
+    image: image2,
+    review: 'I have seen many gyms but Fitclub has one of the best equipments and facilities which makes their plan beginer friendly. ',
+    name: 'JOHN KEVIN',
+    status: 'COACH'
+  },
+  {
+    image : image3,
+    review:'I chose Fitclub over others and I think it was a great decission as they have the best trainers along with great diet and daily routine plan with in an affordable price.',
+    name: 'FRANKLIN',
+    status: "CUSTOMER"
+  }
+];
